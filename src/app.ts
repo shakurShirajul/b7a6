e86bd732479a96063e28b3676930ed7dab0ae746
Shakur <siirajulisllam@gmail.com>
2026-09-02 09:29:45 +0600

    feat: update Prisma schema configuration, add enums and User model

--- src/app.ts
+++ src/app.ts
@@ -3,4 +3,10 @@ import express, { Application } from "express";
 
 const app: Application = express();
 
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
 export default app;
